8b1c6a499e4e63236a8980c476dc40635076b9b0
김대성 <daesung3725@naver.com>
2023-07-25 19:46:33 +0900

    로드맵 추가

--- src/views/BoardView.vue
+++ src/views/BoardView.vue
@@ -2,8 +2,14 @@
   <PageHeader/>
   <div class="Board">
     <div class="Board-Content">
-      <h1>게시판 페이지입니다</h1>
+      <h2 class="menu-title">&#60;게시판&#47;&#62;</h2>
     </div>
+    <div class = "boardMenu">
+      <router-link to ="/"><p>Q&A</p></router-link>
+      <router-link to ="/"><p>스터디 모집</p></router-link>
+      <router-link to ="/"><p>자유 게시판</p></router-link>
+    </div>
+    
   </div>  
   <PageFooter/>
 </template>
@@ -27,6 +33,19 @@ export default {
     background-color: #F2F2F2;
     width : 100%;
     height: 1300px;
+  }
+  .menu-title {
+    font-size: 40px;
+}
+  .boardMenu{
+    display: flex;
+    justify-content: center;
+  }
+  .boardMenu a{
+    text-decoration: none;
+    color : black;
+    margin: 20px;
+    font-size: 20px;
   }
 
 </style>
--- src/views/LoadMapView.vue
+++ src/views/LoadMapView.vue
@@ -1,9 +1,16 @@
 <template>
     <PageHeader/>
     <div class="LoadMap">
+        <h2 class="menu-title">&#60;로드맵&#47;&#62;</h2>
         <div class="LoadMap-Content">
-        <h1>로드맵 페이지입니다</h1>
+          <div class = "front-end">
+            <p>front-end</p>
+          </div>
+          <div class = "back-end">
+            <p>back-end</p>
+          </div>
         </div>
+        
     </div>
     <PageFooter/>
 </template>
@@ -19,12 +26,34 @@ export default {
 </script>
 
 <style scoped>
-  .LoadMap-Content{
-      padding-top: 100px;
-  }
+
   .LoadMap{
     background-color: #F2F2F2;
     width : 100%;
-    height: 1300px;
+    height: 1024px;
+    padding-top: 100px;
+  }
+  .LoadMap-Content{
+    display: flex;
+    justify-content: center;
+  }
+  .menu-title {
+    font-size: 40px;
+  } 
+  .front-end{
+    width : 500px;
+    height: 500px;
+    margin : 50px;
+    border-radius: 20px;
+    background-image: url("@/assets/frontEnd.jpg");
+    background-size : 650px 500px;
+  }
+  .back-end{
+    width : 500px;
+    height: 500px;
+    margin : 50px;
+    border-radius: 20px;
+    background-image: url("@/assets/backEnd.jpeg");
+    background-size : 650px 500px;
   }
 </style>
