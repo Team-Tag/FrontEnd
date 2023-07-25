9d911db71b110bacabe460d55444ddbe21fd0b04
김대성 <daesung3725@naver.com>
2023-07-25 15:43:46 +0900

    footer 디자인 변경 구현

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -1,5 +1,5 @@
 <template>
-    <footer>
+    <footer :class= "isHomePage ? 'home-footer' : 'footer'">
         <div>
             <p>(24252)강원특별자치도 춘천시 한림대학길 1</p>
             <p>대표 033-248-1000</p>
@@ -10,20 +10,35 @@
 
 <script>
 export default {
-  name: 'PageFooter',
-
+    name: 'PageFooter',
+    computed: {
+        isHomePage() {
+            // 현재 페이지가 홈 페이지인지 확인
+            return this.$route.path === '/';
+        },
+    },
 };
 </script>
 
 <style>
-  footer{
+  .footer{
     width: 100%;
     height: 100px;
     background-color: white;
     padding: 10px 20px 20px 20px;
     text-align: left;
   }
-  footer p{
+  .home-footer{
+    width: 100%;
+    height: 100px;
+    background-color: black;
+    padding: 10px 20px 20px 20px;
+    text-align: left;
+  }
+  .footer p{
     color : #9F9D9C;
+  }
+  .home-footer p{
+    color : white;
   }
 </style>
