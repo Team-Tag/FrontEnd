55c42f89788ab1f29e56635ede382c17d09acc40
김대성 <daesung3725@naver.com>
2023-07-26 14:08:01 +0900

    7월 26일 14시main.js수정

--- src/main.js
+++ src/main.js
@@ -8,10 +8,6 @@ import axios from 'axios'
 const app = createApp(App);
 app.config.globalProperties.$axios = axios;
 
-
-const app=createApp(App);
-app.config.globalProperties.$axios=axios;
-
 app.use(store); //다른 컴포넌트에서  vuexStore의 상태,변이,액션,게터등에 접근하여 사용할 수 있다.
 app.use(router);
 app.mount('#app');
