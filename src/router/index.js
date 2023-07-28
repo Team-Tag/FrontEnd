f7bb62cba10b1c750844b8f0bbac80e876abe94d
김대성 <daesung3725@naver.com>
2023-07-28 10:45:58 +0900

    씨애랑 해커톤

--- src/router/index.js
+++ src/router/index.js
@@ -3,7 +3,6 @@ import Home from '../views/HomeView.vue'
 import SignUp from '../views/SignUpView.vue'
 import Board from '../views/BoardView.vue'
 import LoadMap from '../views/LoadMapView.vue'
-import Member from '../views/MemberView.vue'
 import Message from '../views/MessageView.vue'
 import Schedule from '../views/ScheduleView.vue'
 import FrontEnd from '../views/FrontEnd-LM.vue'
@@ -50,11 +49,6 @@ const routes = [
     path : '/LoadMap/BackEnd/EditBoard',
     name : 'EditBoardBackEnd',
     component : EditBoard,
-  },
-  {
-    path : '/Member',
-    name : 'Member',
-    component : Member
   },
   {
     path : '/Message',
